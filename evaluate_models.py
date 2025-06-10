9ecbcd2bb5384bf11bea0bd986923c6861daae19
TranDucLongAIDHSPKTHY <tranduclong29032003@gmail.com>
2025-06-11 00:28:15 +0700

    Hoàn thành ĐATN

--- evaluate_models.py
+++ evaluate_models.py
@@ -2,10 +2,10 @@ import os
 
 print("Evaluate models...")
 
-os.system("python evaluate_models/evaluate_content_based.py")
 os.system("python evaluate_models/evaluate_collaborative.py")
 os.system("python evaluate_models/evaluate_hybrid_sh.py")
 os.system("python evaluate_models/evaluate_autoencoder.py")
 os.system("python evaluate_models/evaluate_ncf")
+os.system("python evaluate_models/evaluate_content_based.py")
 
-print("Đã đánh giá xong 3 mô hình ML!")
+print("Đã đánh giá xong 5 mô hình!")
